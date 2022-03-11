5af66b20c4e86d9550289607d2f3b100dd53018c
Vinh <vinhtq279@gmail.com>
2022-03-11 14:36:03 +0000

    Integrated login then go to home page but havent deployed cookie

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ldap: {
+    dn: 'dc=vnpt-technology,dc=vn',
+    url: 'ldap://ad01.vnpt-technology.vn'
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,104 @@ const DATABASE = require('./utilities/createDB');
 const TABLES = require('./utilities/createTables');
 const cred = require('./utilities/credentials');
 
+const cookieSession = require('cookie-session');
+
+
+const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const CustomStrategy = require('passport-custom').Strategy;
+const { authenticate } = require('ldap-authentication');
+
+/**
+ * Create the passport custom stragegy and name it `ldap`
+ * 
+ * Only this part is where we use ldap-authentication to do
+ * the authentication.
+ * 
+ * Everything else in this example is standard passport staff.
+ *
+ */ 
+passport.use('ldap', new CustomStrategy(
+  async function (req, done) {
+    try {
+      if (!req.body.username || !req.body.password) {
+        throw new Error('username and password are not provided')
+      }
+      // construct the parameter to pass in authenticate() function
+      let ldapBaseDn = 'dc=vnpt-technology,dc=vn';
+      let options = {
+        ldapOpts: {
+          url: 'ldap://ad01.vnpt-technology.vn'
+        },
+        // note in this example it only use the user to directly
+        // bind to the LDAP server. You can also use an admin
+        // here. See the document of ldap-authentication.
+        // userDn: `uid=${req.body.username},${ldapBaseDn}`,
+	adminDn: 'cn=Administrator,cn=Users,dc=vnpt-technology,dc=vn',
+	adminPassword: 'Ns1@an5v',
+	userPassword: req.body.password,
+	userSearchBase: 'ou=O365,ou=VNPTTech,dc=vnpt-technology,dc=vn',
+	usernameAttribute: 'uid',
+	username: req.body.username,
+        //userPassword: req.body.password,
+        //userSearchBase: ldapBaseDn,
+        //usernameAttribute: 'uid',
+        //username: req.body.username
+      }
+      // ldap authenticate the user
+//      let user = await authenticate(options);
+      var user = await authenticate(options);
+      // success
+      console.log(user.uid);
+      done(null, user)
+    } catch (error) {
+      // authentication failure
+      done(error, null)
+    }
+  }
+))
+
+// passport requires this
+passport.serializeUser(function (user, done) {
+  done(null, user);
+})
+
+
+// passport requires this
+passport.serializeUser(function (user, done) {
+  done(null, user);
+})
+// passport requires this
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+})
+// passport requires a session
+var sessionMiddleWare = cookieSession({
+  name: 'session',
+  keys: ['keep the secret only to yourself'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+})
+
+
 class LIBRARY {
 
     constructor(port, app) {
 
         this.port = port;
         this.app = app;
-        this.app.use(express.json())
+        this.app.use(express.json());
+
+	// The order of the following middleware is very important for passport!!
+        this.app.use(bodyParser.urlencoded({ extended: true }))
+        this.app.use(sessionMiddleWare)
+        // passport requires these two
+        this.app.use(passport.initialize())
+        this.app.use(passport.session())
+
+        // web page template
+        this.app.set('view engine', 'pug')
+
         this.temp = 0;
 
         //Initialize Database
@@ -24,11 +115,47 @@ class LIBRARY {
             database: 'library'
         });
 
-    }
+    };
+
 
     get() {
+	// user post username and password
+	this.app.post('/login', passport.authenticate('ldap', { failureRedirect: '/login' }), function (req, res) {
+	//    res.redirect('/success');
+	    res.redirect('http://172.24.104.83:3000');
+  		}
+	)
 
-        //GET LIST OF ALL THE bookS
+	// success page
+	this.app.get('/success', (req, res) => {
+	  	let user = req.user
+		  if (!user) {
+		    res.redirect('/')
+		    return
+		  }
+  		res.render('success', {
+		      userDisplayName: user.cn,
+		      userObject: JSON.stringify(user, null, 2)
+    		})
+	})
+
+	// passport standard logout call.
+	this.app.get('/logout', (req, res) => {
+		  req.logout();
+		  res.redirect('/');
+	})
+
+	// the login page
+	this.app.get('/', function (req, res) {
+		  res.render('index', { title: 'Hey', message: 'Hello there!' })
+	})
+
+
+
+
+
+
+        //GET LIST OF ALL THE BOOKS
         this.app.get('/api/getBooks', (req, res) => {
             let sql = `SELECT * FROM book`;
             this.db.query(sql, (err, result) => {
@@ -40,7 +167,7 @@ class LIBRARY {
             });
         });
 
-        //GET LIST OF bookS BY SEMESTER
+        //GET LIST OF BOOKS BY SEMESTER
         this.app.get('/api/getBooks/:id', (req, res) => {
             let sql = `SELECT * FROM book where semester = '${req.params.id}'`;
             this.db.query(sql, (err, result) => {
@@ -52,11 +179,12 @@ class LIBRARY {
             });
         });
 
-        //borrow A book
+        //BORROW A BOOK
         this.app.post('/api/borrow', (req, res) => {
-            let sql = [`INSERT INTO borrow(idStudent, idBook) VALUES (${req.body.sid}, ${req.body.id});`,
-                       `Update book SET count = count - 1 WHERE id = ${req.body.id}`];
-
+            let sql = [`INSERT INTO BORROW(idStudent, idBook) VALUES (${req.body.sid}, ${req.body.id});`,
+                       `Update BOOK SET count = count - 1 WHERE id = ${req.body.id}`];
+//	    let sql = [`INSERT INTO BORROW(idStudent, idBook) VALUES (${user.uid}, ${req.body.id});`,
+//                       `Update BOOK SET count = count - 1 WHERE id = ${req.body.id}`];
                 for(let i = 0; i < sql.length; i++){
                     this.db.query(sql[i], (err, result) => {
                         if(err){
@@ -71,7 +199,7 @@ class LIBRARY {
                 }
         });
 
-        //GET ALL THE ISSUED bookS BY A student
+        //GET ALL THE ISSUED BOOKS BY A STUDENT
         this.app.get('/api/getIssues/:sid', (req, res) => {
             
             let sql = `SELECT book.name, book.author, book.semester, book.id, borrow.date, borrow.deadline, student.name as sname\
@@ -87,13 +215,13 @@ class LIBRARY {
             });
         });
 
-        //RETURN A book, UPDATE FINE IF ANY
+        //RETURN A BOOK, UPDATE FINE IF ANY
         this.app.post('/api/return', (req, res) => {
             
             let sql = [`SELECT deadline from borrow\
                         WHERE idBook = ${req.body.id} and idStudent = ${req.body.sid}`,
                        `DELETE FROM borrow where idStudent = ${req.body.sid} and idBook = ${req.body.id}`,
-                       `UPDATE book SET count = count + 1 WHERE id = ${req.body.id}`];
+                       `UPDATE BOOK SET count = count + 1 WHERE id = ${req.body.id}`];
 
             for(let i = 0; i < sql.length; i++){
                 this.db.query(sql[i], (err, result) => {
@@ -108,7 +236,7 @@ class LIBRARY {
                         const timeDiff = d2 - d1;
                         const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
                         if(daysDiff > 0) {
-                            this.db.query(`UPDATE student SET fine = fine + ${(daysDiff - 1) * 10} WHERE id = '${req.body.sid}'`, (err, result) => {
+                            this.db.query(`UPDATE STUDENT SET fine = fine + ${(daysDiff - 1) * 10} WHERE id = '${req.body.sid}'`, (err, result) => {
                                 if(err)
                                     console.log(err);
                                 else
@@ -121,7 +249,7 @@ class LIBRARY {
             }
         });
 
-        //GET ALL THE studentS WHO HAVE ISSUED A PARTICULAR book
+        //GET ALL THE STUDENTS WHO HAVE ISSUED A PARTICULAR BOOK
         this.app.get('/api/students/:id', (req, res) => {
             
             let sql = `SELECT student.name, borrow.date, borrow.deadline\
